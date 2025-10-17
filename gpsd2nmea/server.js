236caedff89aa23cc10455552204370b0fda217b
NeoAtlantis <aurichalka@gmail.com>
2025-10-17 23:37:31 +0200

    save

--- gpsd2nmea/server.js
+++ gpsd2nmea/server.js
@@ -1,7 +1,35 @@
 const NMEAEmitter = require("./NMEAEmitter.js");
+const GPS = require("gps");
+const express = require("express");
+const _ = require("lodash");
 
 const nmea_emitter = new NMEAEmitter();
+const gps = new GPS();
+const app = express();
+
+app.use(express.json());
 
 nmea_emitter.on("data", (data)=>{
-	console.log(data);
-})
+	let lines = data.split("\n");
+	for(let l of lines){
+		gps.update(l);
+		console.log(l);
+	}
+});
+
+app.all("/", (req, res)=>{
+	const lat = _.get(gps.state, "lat"),
+		  lng = _.get(gps.state, "lon");
+
+	if(_.isFinite(lat) && _.isFinite(lng)){
+		const ret = {
+		    "location": { lat, lng },
+		    "accuracy": 0.0,
+		};
+		res.status(200).send(ret);
+	} else {
+		res.sendStatus(503);
+	}
+});
+
+app.listen(12947);
